dcf70c7682b2c4121525f057572ec102bc5d57e3
Albert Tian <atian@berkeley.edu>
2022-12-02 16:21:50 -0800

    content: file upload cleanup

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Code, Text } from "@chakra-ui/react";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 import { FormEvent, useState } from "react";
 import { Container } from "../components/Container";
@@ -10,30 +10,23 @@ import { Main } from "../components/Main";
 const API_HOST = "http://127.0.0.1:8000";
 
 const Index = () => {
-  const [data, setData] = useState(null);
   const [file, setFile] = useState(null);
 
-  // const handleFileSubmit = (e: FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   console.log("event");
-  //   console.log(e);
-  //   console.log("event.target");
-  //   console.log(e.target);
-  // };
-
-  //when upload button clicked
+  // Upload file
   const handleFileSubmit = async (e) => {
-    e.preventDefault()
-    console.log(file[0].name);
+    e.preventDefault();
     const formdata = new FormData();
-    formdata.append("file", file[0]);
-    const headers = { "Content-Type": file[0].type };
+    formdata.append("file", file);
+    const headers = {
+      "Content-Type": file.type,
+    };
     await axios.post(`${API_HOST}/uploadfile`, formdata, headers).then();
   };
-  // this is when file has been selected
+  
+  // Change state
   function handleChange(e) {
-    console.log(e.target.files)
-    setFile(e.target.files); //store uploaded file in "file" variable with useState
+    console.log(e.target.files);
+    setFile(e.target.files[0]); //store uploaded file in "file" variable with useState
   }
   return (
     <Container height="100vh">
@@ -43,51 +36,16 @@ const Index = () => {
           Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{" "}
           <Code>TypeScript</Code>.
         </Text>
-        {/* <Button>
-        <FileDrop onTargetClick={onTargetClick} children={""} />
+        {/* TODO: USE CHAKRA */}
+        <form onSubmit={(e) => handleFileSubmit(e)}>
           <input
-            onChange={onFileInputChange}
-            ref={fileInputRef}
+            onChange={handleChange}
+            name="files"
             type="file"
-            className="hidden"
+            accept=".csv"
           />
-        </Button>
-        <Button onClick={handleButtonClick}>upload file</Button> */}
-
-        <form
-          // action={`${API_HOST}/uploadfile/`}
-          // encType="multipart/form-data"
-          // method="post"
-          onSubmit={(e) => handleFileSubmit(e)}
-        >
-          <input onChange={handleChange} name="files" type="file" />
           <input type="submit" />
         </form>
-
-        {/* <List spacing={3} my={0} color="text">
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="green.500" />
-            <ChakraLink
-              isExternal
-              href="https://chakra-ui.com"
-              flexGrow={1}
-              mr={2}
-            >
-              Chakra UI <LinkIcon />
-            </ChakraLink>
-          </ListItem>
-          <ListItem>
-            <ListIcon as={CheckCircleIcon} color="green.500" />
-            <ChakraLink
-              isExternal
-              href="https://nextjs.org"
-              flexGrow={1}
-              mr={2}
-            >
-              Next.js <LinkIcon />
-            </ChakraLink>
-          </ListItem>
-        </List> */}
       </Main>
 
       <DarkModeSwitch />

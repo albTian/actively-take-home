876556b169a8649ddc5149aa158206845a891744
Albert Tian <atian@berkeley.edu>
2022-12-02 16:39:12 -0800

    content: can parse file into pandas

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from typing import Union
 
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import csv
+import codecs
+import pandas as pd
 
 app = FastAPI()
 
@@ -21,6 +24,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 class Item(BaseModel):
     name: str
@@ -42,8 +46,12 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def update_item(item_id: int, item: Item):
     return {"item_name": '{item.name} dog', "item_id": item_id}
 
+
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    print("this is the uploaded file:")
-    print(file.filename)
-    return {"filename": file.filename}
+async def create_upload_file(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+    dog = pd.read_csv(file.file)
+    print(dog)
+
+
+    background_tasks.add_task(file.file.close)
+    return {"message": f"Successfully uploaded {file.filename}"}
